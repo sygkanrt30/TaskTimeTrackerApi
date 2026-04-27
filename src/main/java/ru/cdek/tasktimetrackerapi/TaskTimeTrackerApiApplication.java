f6ef23099372e1166d3ace74d5bc9b3fdec379fd
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2026-04-27 20:18:26 +0300

    init

---
+++ src/main/java/ru/cdek/tasktimetrackerapi/TaskTimeTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package ru.cdek.tasktimetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTimeTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTimeTrackerApiApplication.class, args);
+    }
+
+}
